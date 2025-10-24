dfcd66c8777c6ed70fb23eb148bf87a91a22016a
jooh <johnn55@naver.com>
2025-10-24 14:26:44 +0900

    Feat: 호스트네임 표시 기능 추가
    debug 모드일 때 컴퓨터 이름을 표시하도록 구현

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '홈 페이지입니다.'
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    return render_template('index.html', computername=hostname)
 
 @app.route('/menu')
 def menu():
