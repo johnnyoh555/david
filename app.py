9522a6aa17d18f4dc311ac0cd22dd3d3832b4175
jooh <johnn55@naver.com>
2025-11-27 13:42:05 +0900

    change to test3

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def index():
 def menu():
     return render_template('menu.html')
 
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
+@app.route("/test3")
+def test3():
+    return render_template('test3.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
