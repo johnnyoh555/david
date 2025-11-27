5b1d5ef6fe3f485ff5dcb5ea388773ea98bb056d
오종근 <71632378+johnnyoh555@users.noreply.github.com>
2025-11-27 13:02:49 +0900

    Add new route for test2 page

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def index():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
